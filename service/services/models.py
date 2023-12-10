e774cddea7a0630956be21c218eb99cd82133c8d
VladRosi <a3714152241a@gmail.com>
2023-12-10 15:07:48 +0300

    add celery-singleton and 2 test workers

--- service/services/models.py
+++ service/services/models.py
@@ -1,11 +1,25 @@
+from collections.abc import Iterable
 from django.db import models
 from django.core.validators import MaxValueValidator
 from clients.models import Client
+from services.tasks import set_comment, set_price
 
 
 class Service(models.Model):
   name = models.CharField(max_length=64)
   full_price = models.PositiveIntegerField()
+
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.__full_price = self.full_price
+
+  def save(self, *args, **kwargs):
+    if self.full_price != self.__full_price:
+      self.__full_price = self.full_price
+      for subscription in self.subscriptions.all():
+        set_price.delay(subscription.id)
+        set_comment.delay(subscription.id)
+    return super().save(*args, **kwargs)
 
   def __str__(self):
     return f"{self.name}: {self.full_price}."
@@ -21,6 +35,18 @@ class Plan(models.Model):
   plan_type = models.CharField(choices=PLAN_TYPES, max_length=15)
   discount_percent = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(100)])
 
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.__discount_percent = self.discount_percent
+
+  def save(self, *args, **kwargs):
+    if self.discount_percent != self.__discount_percent:
+      self.__discount_percent = self.discount_percent
+      for subscription in self.subscriptions.all():
+        set_price.delay(subscription.id)
+        set_comment.delay(subscription.id)
+    return super().save(*args, **kwargs)
+
   def __str__(self):
     return f"{self.plan_type.upper()}: {self.discount_percent}%."
 
@@ -29,6 +55,13 @@ class Subscription(models.Model):
   client = models.ForeignKey(Client, on_delete=models.PROTECT, related_name='subscriptions')
   service = models.ForeignKey(Service, on_delete=models.PROTECT, related_name='subscriptions')
   plan = models.ForeignKey(Plan, on_delete=models.PROTECT, related_name='subscriptions')
+  price = models.PositiveIntegerField(default=0)
+  comment = models.CharField(max_length=255, default='')
+
+  # def save(self, *args, save_model=True, **kwargs):
+  #   if save_model:
+  #     set_price.delay(self.id)
+  #   return super().save(*args, **kwargs)
 
   def __str__(self):
     return f"Client: {self.client.user.username}, Service: {self.service.name}, Plan: “{self.plan}”, Finally Prise: {(self.service.full_price * self.plan.discount_percent / 100)}"
--- service/services/views.py
+++ service/services/views.py
@@ -13,8 +13,9 @@ class SubscriptionView(ReadOnlyModelViewSet):
     'plan',
     Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name', 'user__email')),
     # Prefetch('service', queryset=Service.objects.all().only('full_price'))
-  ).annotate(price=F('service__full_price') - \
-    (F('service__full_price') * (F('plan__discount_percent') / 100.00)))
+  )#.annotate(price=F('service__full_price') - \
+    #(F('service__full_price') * (F('plan__discount_percent') / 100.00)))
+    
   serializer_class = SubscriptionSerializer
 
   def list(self, request, *args, **kwargs):
